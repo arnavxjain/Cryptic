e0c09664d4b2985888593092b573b8a5fcd9f0d7
Arnav Jain <jainarnav@outlook.com>
2021-12-29 00:00:10 +0530

    started working on contracts

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "./components/Sidebar"
+
+const App = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <header className="header">
+        <h1>Cryptic</h1>
+        <a href="">Learn More</a>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <span>Web 3.0</span>
+            <span>Ethereum</span>
+            <span>Solidity</span>
+            <span>React</span>
+        </div>
+    )
+}
+
+export default Sidebar;
